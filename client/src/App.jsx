f120b1053c774730e28505d584d240e8c780d071
Humayra Binte Kazal <humayrabintekazal@gmail.com>
2026-03-05 23:47:16 +0600

    Added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import { Route, Routes } from 'react-router';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
 
 // AponKhoj App
 function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Routes>
-        <Route path={'/'} element={<div>Home</div>} />
-        {/* Add your routes here */}
-      </Routes>
+      <main className="flex-grow">
+        <Routes>
+          <Route path={'/'} element={<div></div>} />
+          {/* Add your routes here */}
+        </Routes>
+      </main>
+      <Footer />
       <Toaster
         position="top-center"
         toastOptions={{
@@ -20,7 +24,7 @@ function App() {
           },
         }}
       />
-    </>
+    </div>
   );
 }
 
