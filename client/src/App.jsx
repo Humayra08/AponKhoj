c031a6feb49187381ede0b866e73cd244a8555e2
Moufee Al Doushari <doushari.dipto212@gmail.com>
2026-03-06 23:42:13 +0600

    remove unused routes for user dashboard and verification pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,7 @@ import ScrollToTop from './Components/ScrollToTop';
 import HomePage from './views/HomePage';
 import LoginPage from './views/LoginPage';
 import RegistrationPage from './views/RegistrationPage';
-import UserDashboardPage from './views/UserDashboardPage';
 import ForgotPasswordPage from './views/ForgotPassword';
-import RegistrationVerificationPage from './views/RegistrationVerificationPage';
-import TermsPage from './views/TermsPage'; 
 import './index.css';
 import { Toaster } from 'react-hot-toast';
 import ReportFoundPage from './views/ReportFoundPage';
@@ -25,11 +22,8 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegistrationPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="/terms" element={<TermsPage />} />
-          <Route path="/dashboard" element={<UserDashboardPage />} />
-
-           <Route path="/verify-email" element={<RegistrationVerificationPage />} />
-          {/* Add more routes here */}
+          <Route path="/report-found" element={<ReportFoundPage />} />
+          {/* Add your routes here */}
         </Routes>
       </main>
       <Footer />
